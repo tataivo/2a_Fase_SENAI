780de81270d295453a6e5e27a207f6738772f719
tataivo <thaisivofranca@gmail.com>
2024-08-23 17:15:42 -0300

    Criação do primeiro projeto com react

---
+++ IntroducaoReact/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>Dinossauros</h1>
+    <h2>Carnívoros:</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque quibusdam quia, ducimus mollitia autem, facere asperiores tempore earum consequuntur, ipsum ad ab rerum quod! Culpa vel eos perspiciatis consequuntur aspernatur.</p>
+    </>
+  )
+}
+
+export default App
